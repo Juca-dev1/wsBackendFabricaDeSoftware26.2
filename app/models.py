095a5427a4fe98ff4d0e93df8c01b86ae3ed5758
Juca-dev1 <jucianoliveira001@gmail.com>
2026-08-29 01:18:37 -0300

    feat: cria modelos do sistema de jogos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Genero(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Desenvolvedora(models.Model):
+    nome = models.CharField(max_length=150)
+    pais = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Jogo(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField(blank=True)
+    data_lancamento = models.DateField()
+    nota = models.DecimalField(
+        max_digits=3,
+        decimal_places=1
+    )
+
+    genero = models.ForeignKey(
+        Genero,
+        on_delete=models.CASCADE,
+        related_name='jogos'
+    )
+
+    desenvolvedora = models.ForeignKey(
+        Desenvolvedora,
+        on_delete=models.CASCADE,
+        related_name='jogos'
+    )
+
+    def __str__(self):
+        return self.nome
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
